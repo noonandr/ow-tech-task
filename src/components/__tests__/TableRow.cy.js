7098e11adb7e1b97cf179d5d504f1fb0427399d9
Daniel Noonan <noonan_dr@yahoo.co.uk>
2023-02-27 10:15:50 +0000

    feat(table-sort): added title list view, added intergation of related components

---
+++ src/components/__tests__/TableRow.cy.js
@@ -0,0 +1,23 @@
+import TableRow from "@/components/TableRow.vue";
+
+describe("TableRow", () => {
+  it("playground", () => {
+    cy.mount(TableRow, {
+      props: {
+        titleNumber: "243751",
+        tenure: "Freehold"
+      }
+    });
+  });
+
+  it("renders properly", () => {
+    cy.mount(TableRow, {
+      props: {
+        titleNumber: "243751",
+        tenure: "Freehold"
+      }
+    });
+    cy.get(".title-number").should("contain", "243751");
+    cy.get(".tenure").should("contain", "Freehold");
+  });
+});
