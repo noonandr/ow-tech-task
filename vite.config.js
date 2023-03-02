a6b7cd823ce96444e2050d5a0bb343855398a7a7
Daniel Noonan <noonan_dr@yahoo.co.uk>
2023-03-02 08:24:01 +0000

    feat(docker): adding Docker setup to project

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    port: 5177
+  },
   css: {
     preprocessorOptions: {
       scss: {
